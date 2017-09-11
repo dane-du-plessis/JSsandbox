df44477a576bcd74543db26cafdabddae2d74536
Dane du Plessis <daneasc@gmail.com>
2017-09-11 11:52:26 +0200

    Simple array and table exercise

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-var i = 100;
-var msg = '';
-while(i > 0) {
-	msg = msg + ' ' + i;
-	
-	switch (i%3) {
-		case 0:
-			msg += "";
-			break;
-		case 1:
-			msg += "#";
-			break;
-		case 2:
-			msg += "##";
-			break;
-	}
-	
-	--i;
+var arr = new Array();
+
+document.write("<table border = \"1\">");
+
+arr[0] = "Tom";
+arr[1] = "Dick";
+arr[2] = "Harry";
+arr[3] = "Jefferson";
+arr[4] = "Peterson";
+arr[5] = "Jackson";
+arr[6] = "Johnson";
+
+for (var i = 0; i < arr.length; i++)
+{
+	document.write(
+	"<tr><td>" + i + "</td><td>" + arr[i] +"</td></tr>");
 }
 
-alert(msg);
+
+
+document.write("</table>");
