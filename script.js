3263d79bcf907aab134a66e264d8efae6be6261f
Dane du Plessis <daneasc@gmail.com>
2017-09-11 12:25:10 +0200

    Simple array manipulation, with a sorting issue.

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-var arr = new Array();
+var ages = new Array(12,4,34,7,2,3,41,5,17,8,24,19,16,17,18,21);
+var persons = ages.length;
 
-document.write("<table border = \"1\">");
+var adults = new Array();
+var minors = new Array();
 
-arr[0] = "Tom";
-arr[1] = "Dick";
-arr[2] = "Harry";
-arr[3] = "Jefferson";
-arr[4] = "Peterson";
-arr[5] = "Jackson";
-arr[6] = "Johnson";
-
-for (var i = 0; i < arr.length; i++)
-{
-	document.write(
-	"<tr><td>" + i + "</td><td>" + arr[i] +"</td></tr>");
+while (ages.length) {
+	var age = ages.pop();
+	if (age >= 18) adults.push(age);
+	else minors.push(age);
 }
 
+adults = adults.sort();
+adults = adults.reverse();
+minors = minors.sort();
+minors = minors.reverse();
 
-
-document.write("</table>");
+document.write("There are " + persons + " persons. " + "<br />");
+document.write("There are " + adults.length + " adults with ages " + adults.join(", " )+ ".<br />");
+document.write("There are " + minors.length + " minors with ages " + minors.join(", " )+ ".<br />");
