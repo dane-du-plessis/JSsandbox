36cb2f2f770190cae65ecde09b1dbab64a9bc637
Dane du Plessis <daneasc@gmail.com>
2017-09-16 13:41:41 +0200

    All working except for expiration date check.

--- functionScript.js
+++ functionScript.js
@@ -2,11 +2,36 @@ function validateInput() {
 	
 	alert("Checking input data.");
 	var isValid = false;
-	//var CN = document.MyForm.CardNumber.value;
 	
-	if (document.MyForm.CardNumber.value !== ""){
-		isValid = true;
+	isValid = evalCardType();
+	if (isValid == false) {
+		alert("Card type not specified.");
 	}
-	if (isValid==false) alert("Card Number is invalid");
+	
+	if (document.MyForm.CardNumber.value == ""){
+		alert("Card Number invalid");
+		isValid = false;
+	}
+	
+	if (document.MyForm.MM.value <= 5 && document.MyForm.YYYY.value <= 2018) {
+		alert("Card  has expired");
+		isValid = false;
+	}
+	
+	if (document.MyForm.CardHolderName.value == ""){
+		alert("Card  holder name invalid");
+		isValid = false;
+	}
+	
+	
 	return isValid;
+}
+
+function evalCardType() {
+	var group = document.MyForm.CardType;
+	for (var i = 0; i < group.length; i++) {
+		if (group[i].checked) break;
+	}
+	if (i == group.length) return false;
+	else return true;
 }
